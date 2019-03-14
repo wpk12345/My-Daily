dca366dce81732764bc177bc500e481ed802cf66
William King <wpk12345@gmail.com>
2019-03-14 02:39:47 -0400

    finished auth flow

--- README.md
+++ README.md
@@ -3,4 +3,8 @@ A website I built that has everything I need in one place
 
 ## Following Technologies Used:
 
-#### React/Redux/ES6/CSS/SASS/Mongo/Mongoose/Express
+#### React/Redux/ES6/CSS/SASS/Mongo/Mongoose/Express/Passport for Google OAuth
+
+## APIs Used:
+
+####NYTimes/OpenWeather/Twit(Twitter)/YouTube
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,20 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  //user.id is mongo id not googleid.  in case we use different passport strategies.  besides googleid is only for auth.  after authentication we only care about mongo id.
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +24,15 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken", accessToken);
-      console.log("refreshToken", refreshToken);
-      console.log("profile", profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
